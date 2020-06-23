1c27d8e2aaeea5b258ba7aed3cd666a2e8b285fa
Zlatko Nikolovski <Zlatko.Nikolovski@katoennatie.com>
2020-06-23 12:56:04 +0200

    add EfCoreJsonValueConverter

--- ProofOfConceptOrders/Startup.cs
+++ ProofOfConceptOrders/Startup.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProofOfConceptOrders.InvoicingDbContext;
+using System;
 
 namespace ProofOfConceptOrders
 {
@@ -18,6 +21,7 @@ namespace ProofOfConceptOrders
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Add(new ServiceDescriptor(typeof(InvoicingContext), new InvoicingContext()));   
             services.AddControllers();
         }
 
