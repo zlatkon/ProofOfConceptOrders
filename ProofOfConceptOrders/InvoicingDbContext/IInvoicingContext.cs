0ccd7c6069d4891d82d7e721d4b7d8fc542125a1
Zlatko Nikolovski <Zlatko.Nikolovski@katoennatie.com>
2020-06-23 13:23:59 +0200

    update

--- ProofOfConceptOrders/InvoicingDbContext/IInvoicingContext.cs
+++ ProofOfConceptOrders/InvoicingDbContext/IInvoicingContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProofOfConceptOrders.Model;
+using ProofOfConceptOrders.Model.ValueObject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
