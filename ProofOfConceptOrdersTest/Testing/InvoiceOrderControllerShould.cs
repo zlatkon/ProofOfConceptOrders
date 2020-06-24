81248d0451268a4e48a5ef1193290159f68e35a9
Zlatko Nikolovski <Zlatko.Nikolovski@katoennatie.com>
2020-06-24 09:26:41 +0200

    update test

--- ProofOfConceptOrdersTest/Testing/InvoiceOrderControllerShould.cs
+++ ProofOfConceptOrdersTest/Testing/InvoiceOrderControllerShould.cs
@@ -30,6 +30,33 @@ namespace ProofOfConceptOrdersTest.Testing
             order2.UpdateOrderType("Inbound");
             order2.SetSite("Lb1227");
 
+            var action1 = order1.AddAction("Action1");
+            var action2 = order1.AddAction("Action2");
+            var action3 = order1.AddAction("Action3");
+            var action4 = order2.AddAction("Action1");
+            var action5 = order2.AddAction("Action2");
+            var action6 = order2.AddAction("Action3");
+
+            var property1 = order1.AddProperties("property1", "PropertyValue1");
+            var property2 = order1.AddProperties("property2", "PropertyValue2");
+            var property3 = order1.AddProperties("property3", "PropertyValue3");
+            var property4 = order2.AddProperties("property1", "PropertyValue1");
+            var property5 = order2.AddProperties("property2", "PropertyValue2");
+            var property6 = order2.AddProperties("property3", "PropertyValue3");
+
+
+            for (int i = 1; i <= 10; i++)
+            {
+                action1.AddProperty(Guid.NewGuid(), "string", $"name{i}", "$value{i}");
+                action2.AddProperty(Guid.NewGuid(), "string", $"name{i}", "$value{i}");
+                action3.AddProperty(Guid.NewGuid(), "string", $"name{i}", "$value{i}");
+
+                action4.AddProperty(Guid.NewGuid(), "string", $"name{i}", "$value{i}");
+                action5.AddProperty(Guid.NewGuid(), "string", $"name{i}", "$value{i}");
+                action6.AddProperty(Guid.NewGuid(), "string", $"name{i}", "$value{i}");
+            }
+            
+
             await InsertAsync(order1);
             await InsertAsync(order2);
 
